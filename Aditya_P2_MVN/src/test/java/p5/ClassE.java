ea01f7244b4f46eee39202735fb262d8563553c7
saiadityan <saiadityan18@gmail.com>
2023-01-26 12:52:21 +0530

    New package added

---
+++ Aditya_P2_MVN/src/test/java/p5/ClassE.java
@@ -0,0 +1,22 @@
+package p5;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassE 
+{
+	@Test
+	  public void tc011() 
+	  {
+		  Reporter.log("tc009 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc0012() 
+	  {
+		  Reporter.log("tc010 executed successfull", true);
+	  }
+
+	  
+
+}
