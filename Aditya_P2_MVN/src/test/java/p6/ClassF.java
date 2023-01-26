4d74f0acf93ca72454b4241c6be8ba33a1a8233a
saiadityan <saiadityan18@gmail.com>
2023-01-26 12:56:19 +0530

    new pkg update

---
+++ Aditya_P2_MVN/src/test/java/p6/ClassF.java
@@ -0,0 +1,20 @@
+package p6;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassF 
+{
+	@Test
+	  public void tc013() 
+	  {
+		  Reporter.log("tc013 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc0014() 
+	  {
+		  Reporter.log("tc014 executed successfull", true);
+	  }
+
+}
